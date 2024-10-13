6fe45dae1c778ade34dd2630d7de7c027ba92f62
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-10-14 00:02:21 +0530

    Add files via upload

---
+++ Shapes Patterns/Pattern30.java
@@ -0,0 +1,27 @@
+/*
+ 
+        * 
+ * 
+  * 
+   * 
+    * 
+
+ */
+
+public class Pattern30 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j >= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("_");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
